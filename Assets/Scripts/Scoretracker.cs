24a69f975dfe49454552ee3be033358fb28b148f
Kenneth Muuli <kenneth.muuli@gmail.com>
2024-01-05 11:13:06 +0200

    Added TimeScoreTicks to Scoretracker.cs for adding score based on player alive time

--- Assets/Scripts/Scoretracker.cs
+++ Assets/Scripts/Scoretracker.cs
@@ -3,6 +3,9 @@ using UnityEngine;
 public class Scoretracker : MonoBehaviour
 {
     [SerializeField] private int currentScore = 0;
+    [SerializeField] private int scorePlusAtTick;
+    [SerializeField] private float secondsBetweenTicks;
+    private float nextTickTime;
 
     // This function is called when the object becomes enabled and active.
     private void OnEnable() {
@@ -16,8 +19,22 @@ public class Scoretracker : MonoBehaviour
         ObjectToDestory.objectDestroyed += UpdateScore;
     }
 
+    // Update is called every frame, if the MonoBehaviour is enabled.
+    private void Update() {
+        TimeScoreTicks();
+    }
+
     private void UpdateScore(int scoreToAdd)
     {
         currentScore = currentScore + scoreToAdd;
+    }
+
+    // Used to track and set time ticks for updating the score based on how long the player has been alive for
+    private void TimeScoreTicks() {
+        // Comparing if current time is later than nextTickTime, if yes, set nextTickTime and call UpdateScore
+        if(Time.unscaledTime > nextTickTime) {
+            nextTickTime = Time.unscaledTime + secondsBetweenTicks;
+            UpdateScore(scorePlusAtTick);
+        }
     }
 }
