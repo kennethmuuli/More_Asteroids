3282e51a4642d84f00834d4cc2a332441affb6cc
Kenneth Muuli <kenneth.muuli@gmail.com>
2024-01-21 16:01:31 +0200

    REFACTOR PROJECTILE SCRIPT
    Refactored projectile as it's intent is not the same as a destructile
    object, pollutes event calls. Also moved it to the correct place in the
    scripts directory.

--- Assets/Scripts/Player/Projectile.cs
+++ Assets/Scripts/Player/Projectile.cs
@@ -1,15 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
-public class Projectile : BaseDestructibleObject
+public class Projectile : MonoBehaviour
 {
      // Destroy projectile if it collides with asteroid
     private void OnCollisionEnter2D(Collision2D other) 
     {
         if(other.collider.tag == "Asteroid")
         {
-            Die();
+            Destroy(gameObject);
         }
     }
 }
