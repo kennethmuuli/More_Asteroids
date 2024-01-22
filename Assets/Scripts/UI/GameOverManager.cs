98a7677a6b593f937393a9683b9411a93ff0e9d0
Marko Villemson <marko.villemson@tptlive.ee>
2024-01-22 20:07:19 +0200

    ADD GAMEOVERMANAGER.CS
    GameOver functionality and display final score

---
+++ Assets/Scripts/UI/GameOverManager.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GameOverManager : MonoBehaviour
+{
+    public GameObject gameOverScreen; 
+    public TMP_Text finalScoreText; 
+    public Scoretracker scoretracker; 
+
+    private void OnEnable()
+    {
+        BaseDestructibleObject.objectDestroyed += OnObjectDestroyed;
+    }
+
+    private void OnDisable()
+    {
+        BaseDestructibleObject.objectDestroyed -= OnObjectDestroyed;
+    }
+
+    private void OnObjectDestroyed(int scoreValue)
+    {
+        // Kontrollime, kas hävitatud objekt oli Player, vaadates skoori väärtust
+        // Kui mängija skoori väärtus on 0 
+        if (scoreValue == 0) // mängija skoori väärtus 0, mida saab seadistada Inspectoris
+        {
+            GameOver();
+        }
+    }
+
+    private void GameOver()
+    {
+                gameOverScreen.SetActive(true);
+
+        // Uuendab lõppskoori teksti Scoretracker skripti praeguse skoori põhjal
+        finalScoreText.text = "Final Score: " + scoretracker.GetCurrentScore().ToString();
+    }
+}
