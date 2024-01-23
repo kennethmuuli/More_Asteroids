47fcfe13de1bd82afafa5b6c780207709aac8055
Kenneth Muuli <kenneth.muuli@gmail.com>
2024-01-23 08:27:05 +0200

    MOVE REUSABLE LOGIC TO PARENT CLASS
    Moved all reusable logic to parent class. For now, all that's left in
    the asteroid class is a beautifully clear and human readable list of
    methods detailing its entire behaviour. This logic can easily be
    overridden with asteroid specific unique logic if there was any, but
    currently there isn't.

--- Assets/Scripts/Destructibles/Asteroid.cs
+++ Assets/Scripts/Destructibles/Asteroid.cs
@@ -2,13 +2,6 @@ using UnityEngine;
 
 public class Asteroid : BaseDestructibleObject
 {
-    [Range(0.5f,1f)]
-    [SerializeField] private float minSize = 0.5f;
-    [Range(1f,3f)]
-    [SerializeField] private float maxSize = 1.5f;
-    [Range(10f,200f)]
-    [SerializeField] private float speed = 50.0f;
-    private Rigidbody2D _rigidbody;
 
     // Called when the script instance is being loaded
     private void Awake()
@@ -33,31 +26,6 @@ public class Asteroid : BaseDestructibleObject
 
         DespawnIfInvisible();
     }
-    private void RandomizeSizeAndRotation(){  
-        // Set a random rotation to the asteroid
-        transform.eulerAngles = new Vector3(0.0f, 0.0f, Random.value * 360.0f);
-
-        // Get a random size between the max and min size
-        float size = Random.Range(minSize,maxSize);
-        
-        // Set the scale of the asteroid based on the 'size' variable
-        transform.localScale = Vector2.one * size;
-    }
-
-    // Example method that may trigger the objectDestroyed event
-    private void MoveAndSpin(Vector2 direction, float torque = 2f)
-    {
-        // Add a force to the Rigidbody2D to set the asteroid in motion
-        _rigidbody.AddForce(direction * speed);
-        // Add a spin force to the object, if not set then default value is used
-        _rigidbody.AddTorque(torque);
-    }
-
-    private void DespawnIfInvisible(){
-        if (!GetComponentInChildren<Renderer>().isVisible)
-        {
-            Destroy(gameObject);
-        }
-    }
+    
 
 }
--- Assets/Scripts/Destructibles/BaseDestructibleObject.cs
+++ Assets/Scripts/Destructibles/BaseDestructibleObject.cs
@@ -6,14 +6,47 @@ using UnityEngine;
 public abstract class BaseDestructibleObject : MonoBehaviour
 {
     [SerializeField] protected int myScoreValue;
+    [Range(0.5f,1f)]
+    [SerializeField] protected float minSize = 0.5f;
+    [Range(1f,3f)]
+    [SerializeField] protected float maxSize = 1.5f;
+    [Range(10f,200f)]
+    [SerializeField] protected float speed = 50.0f;
+    protected Rigidbody2D _rigidbody;
     protected bool iGotHit = false;
     public static event Action<int> objectDestroyed;
-
+    
     void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Projectile")
         {
             iGotHit = true;
+        }
+    }
+
+    protected virtual void RandomizeSizeAndRotation(){  
+        // Set a random rotation to the asteroid
+        transform.eulerAngles = new Vector3(0.0f, 0.0f, UnityEngine.Random.value * 360.0f);
+
+        // Get a random size between the max and min size
+        float size = UnityEngine.Random.Range(minSize,maxSize);
+        
+        // Set the scale of the asteroid based on the 'size' variable
+        transform.localScale = Vector2.one * size;
+    }
+
+    protected virtual void MoveAndSpin(Vector2 direction, float torque = 2f)
+    {
+        // Add a force to the Rigidbody2D to set the asteroid in motion
+        _rigidbody.AddForce(direction * speed);
+        // Add a spin force to the object, if not set then default value is used
+        _rigidbody.AddTorque(torque);
+    }
+
+    protected virtual void DespawnIfInvisible(){
+        if (!GetComponentInChildren<Renderer>().isVisible)
+        {
+            Destroy(gameObject);
         }
     }
 
