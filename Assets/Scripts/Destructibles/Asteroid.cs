e8e2982af9ece02a0d63fe04377c695d18016c64
Kenneth Muuli <kenneth.muuli@gmail.com>
2024-01-23 07:35:45 +0200

    ADD RANDOMIZESIZEANDROTATION METHOD TO ASTEROID
    Put the size and rotation randomising into a separate method and
    triggered it from the start method. Easier to read the overall behavior.

--- Assets/Scripts/Destructibles/Asteroid.cs
+++ Assets/Scripts/Destructibles/Asteroid.cs
@@ -3,7 +3,6 @@ using UnityEngine;
 public class Asteroid : BaseDestructibleObject
 {
     // Public variables for asteroid properties
-    public float size = 1.0f;
     public float minSize = 0.5f;
     public float maxSize = 1.5f;
     public float speed = 50.0f;
@@ -22,11 +21,7 @@ public class Asteroid : BaseDestructibleObject
     // Called on the frame when a script is enabled
     private void Start()
     {
-        // Set a random rotation to the asteroid
-        transform.eulerAngles = new Vector3(0.0f, 0.0f, Random.value * 360.0f);
-
-        // Set the scale of the asteroid based on the 'size' variable
-        transform.localScale = Vector3.one * size;
+        RandomizeSizeAndRotation();        
 
         MoveAndSpin(transform.up);
     }
@@ -36,6 +31,16 @@ public class Asteroid : BaseDestructibleObject
         {
             Die();
         }
+    }
+    private void RandomizeSizeAndRotation(){  
+        // Set a random rotation to the asteroid
+        transform.eulerAngles = new Vector3(0.0f, 0.0f, Random.value * 360.0f);
+
+        // Get a random size between the max and min size
+        float size = Random.Range(minSize,maxSize);
+        
+        // Set the scale of the asteroid based on the 'size' variable
+        transform.localScale = Vector2.one * size;
     }
 
     // Example method that may trigger the objectDestroyed event
@@ -48,5 +53,6 @@ public class Asteroid : BaseDestructibleObject
         // Destroy the asteroid after a specified lifetime
         Destroy(gameObject, maxLifetime);
     }
+
 
 }
