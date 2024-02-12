e10463f450253a85ff488f6a8787319bd7bae886
Kenneth Muuli <kenneth.muuli@gmail.com>
2024-02-12 19:53:04 +0200

    REFACTORED PLAYER BASIC PLAYERATTACKS
    Refactored PlayerAttacks to reduce the amount of code and decouple the
    projectile logic from the firing logic. PlayerAttacks now just deals
    with managaging the attacks and does not care about the effects
    afterwards.

--- Assets/Scripts/Player/Projectile.cs
+++ Assets/Scripts/Player/Projectile.cs
@@ -5,7 +5,21 @@ using UnityEngine;
 
 public class Projectile : MonoBehaviour
 {
-     // Destroy projectile if it collides with asteroid
+    // Projectile flight speed 
+    [SerializeField] float projectileSpeed = 10f;
+    // The time after which the object is destroyed 1f = 1s
+    [SerializeField] float projectileLifetime = 5f;
+
+    private void Start() {
+        // Access projectile`s rigidbody2D component
+            Rigidbody2D rb = GetComponent<Rigidbody2D>();
+
+            rb.velocity = transform.up * projectileSpeed;
+
+            Destroy(gameObject,projectileLifetime);
+    }
+    
+    // Destroy projectile if it collides with asteroid
     private void OnCollisionEnter2D(Collision2D other) 
     {
         if(other.collider.tag == "Asteroid")
