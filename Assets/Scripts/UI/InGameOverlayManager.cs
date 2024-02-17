19f2ea28287e215eaa93e0b543b5cd5ebbde6398
Kenneth Muuli <kenneth.muuli@gmail.com>
2024-02-17 09:35:50 +0200

    Refactor class for scalability and cleaner implementation

--- Assets/Scripts/UI/InGameOverlayManager.cs
+++ Assets/Scripts/UI/InGameOverlayManager.cs
@@ -13,11 +13,9 @@ public class InGameOverlayManager : MonoBehaviour
     [Header("Laser PU Display Components")]
     [SerializeField] private GameObject laserPUDisplay; 
     [SerializeField] private Slider laserPUSlider;
-    private float laserPURemainingDuration;
     [Header("Shield PU Display Components")]
     [SerializeField] private GameObject shieldPUDisplay; 
     [SerializeField] private Slider shieldPUSlider;
-    private float shieldPURemainingDuration;
     
     private void OnEnable() {
         Scoretracker.scoreUpdated += OnScoreUpdated;
@@ -29,51 +27,42 @@ public class InGameOverlayManager : MonoBehaviour
         PowerUp.powerUpCollected -= OnPowerUpCollected;
     }
 
-    private void Update() {
-        // FIXME: Dirty implementation here
-        if (Time.time < laserPURemainingDuration)
-        {
-            UpdatePowerUpDurationDisplayBar();  
-            
-        } else laserPUDisplay.SetActive(false);
-        
-        if (Time.time < shieldPURemainingDuration)
-        {
-            UpdatePowerUpDurationDisplayBar2();  
-            
-        } else shieldPUDisplay.SetActive(false);
-    }
-    private void OnPowerUpCollected(PowerUpType type, float duration)
+    private void OnPowerUpCollected(PowerUpType type, float PUDuration)
     {
         switch (type)
         {
             case PowerUpType.Laser:
-                laserPUDisplay.SetActive(true);
-                laserPURemainingDuration = Time.time + duration;
+                StartCoroutine(UpdatePowerUpBar(PUDuration, laserPUSlider, laserPUDisplay));
                 break;
             case PowerUpType.Shield:
-                shieldPUDisplay.SetActive(true);
-                shieldPURemainingDuration = Time.time + duration;
+                StartCoroutine(UpdatePowerUpBar(PUDuration, shieldPUSlider, shieldPUDisplay));
                 break;
             default:
             break;
         }
-        
-        
     }
 
-    // FIXME: Dirty implementation here
-    private void UpdatePowerUpDurationDisplayBar () {
-        laserPUSlider.value = Mathf.Abs(1 - Time.time / laserPURemainingDuration);
-    }
-    // FIXME: Dirty implementation here
-    private void UpdatePowerUpDurationDisplayBar2 () {
-        shieldPUSlider.value = Mathf.Abs(1 - Time.time / shieldPURemainingDuration);
+    IEnumerator UpdatePowerUpBar (float powerupDuration, Slider powerupSlider, GameObject powerupDisplay) {
+        powerupDisplay.SetActive(true);
+        
+        float timeElapsed = 0;
+
+        while (timeElapsed < powerupDuration){
+            float t = timeElapsed / powerupDuration;
+            powerupSlider.value = Mathf.Lerp(1,0,t);
+            timeElapsed += Time.deltaTime;
+
+            yield return null;
+        }
+
+        powerupSlider.value = 0;
+
+        powerupDisplay.SetActive(false);
+        
     }
 
     private void OnScoreUpdated(int scoreToDisplay)
     {
-    // Or should it be in Update method?
         score.text = scoreToDisplay.ToString();
     }
 }
