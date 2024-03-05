bc6c3da3791520a889642e458a9d3b80965fcc2b
Kenneth Muuli <kenneth.muuli@gmail.com>
2024-03-05 16:09:35 +0200

    UPDATE MAIN MENU
    Main menu now navigable with WASD keys, ENTER to submit.
    Merged Main and Option menu managers into MainMenu.
    Refactored AudioManager, ready to act as an AudioManager in-game as
    well, simply add an SFX name and switch case in the AudioManager and
    invoke the playSFX event from where ever you want the sound to play.
    
    MenuDemo scene now repurposed as the Main Menu scene.

--- Assets/Scripts/Audio/AudioManager.cs
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -1,18 +1,99 @@
 using UnityEngine.Audio;
 using UnityEngine;
 using System;
+using Unity.VisualScripting;
 
 public class AudioManager : MonoBehaviour
 {
+    public static AudioManager instance;
+    private float _currentMusicVolume = 0.5f;
     [Header("Audio Source")]
-    [SerializeField] AudioSource musicSource;
-    [SerializeField] AudioSource SFXSource;
+    [SerializeField] private AudioSource musicSource;
+    [SerializeField] private AudioSource SFXSource;
 
-    [Header("Audio Clip")]
-    public AudioClip background;
+    [Header("Background Music")]
+    [SerializeField] private AudioClip _backgroundMusic;
+
+    private AudioClip _SFXToPlay;
+    public static Action<SFXName> playSFX;
+    [Header("In-game Audio Clips")]
+    [SerializeField] private AudioClip _projectileSound;
+    [SerializeField] private AudioClip _laserSound;
+    [SerializeField] private AudioClip _asteroidDestructionSound;
+
+
+
+
+    public float GetCurrentMusicVolume {
+        get {return _currentMusicVolume;}
+    }
+    private void Awake() {
+        //Make a static instance of the audioManager, to avoid more than one of them in any given scene
+        if (instance == null)
+        {
+            instance = this;
+        } else {
+            Destroy(gameObject);
+        } 
+    }
+
+    private void OnEnable() {
+        playSFX += OnPlaySoundEffect;
+    }
+    private void OnDisable() {
+        playSFX -= OnPlaySoundEffect;
+    }
+
     private void Start()
     {
-        musicSource.clip = background;
+        //Add the don't destory on load property so that the audioManager does not get destoryed between scenes
+        DontDestroyOnLoad(gameObject);
+        PlayBackgroundMusic();
+    }
+    public void UpdateMusicVolume (float newVolume) {
+        if(newVolume < 0) {
+            newVolume = 0;
+            Debug.LogWarning("Sent in music volume below 0.0, min value 0, set value to 0");
+        } else if (newVolume > 1)
+        {
+            newVolume = 1;
+            Debug.LogWarning("Sent in music volume above 1.0, max value 1, set value to 1");
+        }
+        _currentMusicVolume = newVolume;
+        musicSource.volume = newVolume;
+    }
+
+    private void PlayBackgroundMusic() {
+        musicSource.clip = _backgroundMusic;
         musicSource.Play();
     }
+
+    // Create a new case for each audio clip and then simply invoke the event from the right place with 'AudioManager.playSFX?.Invoke(<SFXName here>);
+    private void OnPlaySoundEffect (SFXName SFXName) {
+
+        switch (SFXName)
+        {
+            case SFXName.ShootProjectile:
+                _SFXToPlay = _projectileSound;
+                break;
+            case SFXName.ShootLaser:
+                _SFXToPlay = _laserSound;
+                break;
+            case SFXName.AstroidDestruction:
+                _SFXToPlay = _asteroidDestructionSound;
+                break;
+            default:
+                break;
+        }
+
+        SFXSource.clip = _SFXToPlay;
+        SFXSource.Play();
+    }
+}
+
+//If you want to add a new SFX name, simply add it here, this is used for referencing audio clips, which are all added to this script
+public enum SFXName {
+    ShootProjectile,
+    ShootLaser,
+    AstroidDestruction
 }
--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -1,19 +1,61 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
+    [Header("Main Menu")]
+    [SerializeField] private GameObject mainMenu;
+    [SerializeField] private Button mainMenuInitial;
+    [Header("Options Menu")]
+    [SerializeField] private GameObject optionsMenu;
+    [SerializeField] private Button optionsMenuInitial;
+    [SerializeField] private AudioMixer audioMixer;
+    [SerializeField] private Slider musicVolumeSlider;
+
+    private void Start() {
+        // Read in last value from audioManager, otherwise every return to main menu resets volume
+        musicVolumeSlider.value = AudioManager.instance.GetCurrentMusicVolume;
+    }
+
+    public void SetMusicVolume()
+    {
+        float volume = musicVolumeSlider.value;
+        AudioManager.instance.UpdateMusicVolume(volume);
+    }
     public void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
+    public void ActivateMainMenu () {
+        DeactivateMenus();
+        mainMenu.SetActive(true);
+        EventSystem.current.SetSelectedGameObject(mainMenuInitial.gameObject);
+    }
+
+    public void ActivateOptionsMenu () {
+        DeactivateMenus();
+        optionsMenu.SetActive(true);
+        EventSystem.current.SetSelectedGameObject(optionsMenuInitial.gameObject);
+        SetMusicVolume();   
+    }
+
+    private void DeactivateMenus () {
+        mainMenu.SetActive(false);
+        optionsMenu.SetActive(false);
+    }
+
     public void QuitGame()
     {
+#if UNITY_EDITOR
         // for testing
         Debug.Log("QUIT!");
+#endif
 
         Application.Quit();
     }
